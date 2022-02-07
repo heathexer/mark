6cb9b6c1bd7ab13391ad831d02422b302d78d5f8
Heath <heathexer@gmail.com>
2022-02-07 11:57:18 -0800

    add hw config to rust

--- src/rs/src/main.rs
+++ src/rs/src/main.rs
@@ -1,7 +1,21 @@
-use rpi-led-matrix::{LedColor, LedMatrix};
+use rpi_led_matrix::{LedColor, LedMatrix, LedMatrixOptions, LedRuntimeOptions};
 
 fn main() {
-    let matrix = LedMatrix::new(None, None).expect("Failed to create LedMatrix"));
+    // Options
+    let mut options = LedMatrixOptions::new();
+    options.set_hardware_mapping("adafruit-hat");
+    options.set_pixel_mapper_config("V-mapper");
+    options.set_rows(32);
+    options.set_cols(64);
+    options.set_chain_length(2);
+    options.set_parallel(1);
+    options.set_row_addr_type(0) // 0 is default but this was in main.py
+    options.set_multiplexing(0);
+    options.set_limit_refresh(100);
+    let mut rt_options = LedRuntimeOptions::new();
+    rt_options.set_gpio_slowdown(4);
+
+    let matrix = LedMatrix::new(options, rt_options).expect("Failed to create LedMatrix");
     let mut canvas = matrix.offscreen_canvas();
 
     for red in (0..255).step_by(16) {
